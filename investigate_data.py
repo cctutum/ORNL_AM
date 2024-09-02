a41b392ed57476c3e9417e9baf6e3ae8ff38e19a
Cem C. Tutum <cem.celal@gmail.com>
2024-09-02 14:11:09 -0500

    view hdf5 contents

--- investigate_data.py
+++ investigate_data.py
@@ -6,12 +6,18 @@ file = '2021-07-13 TCR Phase 1 Build 1.hdf5'
 path_file = os.path.join(downloads_path, file)
 
 build = h5py.File(path_file, 'r')
-print(list(build.keys()))
+print(f"\nFile name: {file}")
+print(list(build.keys()), '\n')
 
 
-def print_structure(name, obj):
-    print(name)
-    if isinstance(obj, h5py.Dataset):
-        print(f"  Shape: {obj.shape}, Dtype: {obj.dtype}")
-        
-build.visititems(print_structure)
+def preview_hdf5(name, obj):
+	if isinstance(obj, h5py.Group):
+		print(f"Group: {name}")
+	elif isinstance(obj, h5py.Dataset):
+		print(f"Dataset: {name}")
+		print(f"Shape: {obj.shape}, Dtype: {obj.dtype}")
+
+# with h5py.File(path_file, 'r') as f:
+# 	f.visititems(preview_hdf5)
+
+build.visititems(preview_hdf5)
